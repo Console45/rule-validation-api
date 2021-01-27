45fdf23e72b853ac02a838f3e530333cc2b7fac7
Cosmsos <cosmosappiah029@gmail.com>
2021-01-27 20:51:21 +0000

    update

--- src/middlewares/validateRequiredFields.ts
+++ src/middlewares/validateRequiredFields.ts
@@ -2,14 +2,14 @@ import { BadRequest } from "./../utils/apiError";
 import { Request, Response, NextFunction } from "express";
 
 // validate the rule field
-const checkField = (field: any): void => {
+const checkField = (field: string): void => {
   if (!field) throw new BadRequest("rule field is required.", null);
   if (typeof field !== "string")
     throw new BadRequest("rule field should be a string.", null);
 };
 
 // validate the rule condition field
-const checkCondition = (condition: any): void => {
+const checkCondition = (condition: string): void => {
   if (!condition) throw new BadRequest("rule condition is required.", null);
   if (!/\beq\b|\bcontains\b|\bgte\b|\bneq\b|\bgt\b/g.test(condition))
     throw new BadRequest(
@@ -19,7 +19,7 @@ const checkCondition = (condition: any): void => {
 };
 
 // validate the rule condition value field
-const checkConditionValue = (condition_value: any): void => {
+const checkConditionValue = (condition_value: number): void => {
   if (!condition_value)
     throw new BadRequest("rule condition value is required.", null);
   if (typeof condition_value !== "number")
@@ -41,14 +41,41 @@ const validateData = (data: any): any => {
   if (!data) throw new BadRequest("data is required.", null);
 };
 
+// check if fields in rule objects exist in data
+const checkFields = (field: string, data: any) => {
+  //field validation error
+  const fieldError: BadRequest = new BadRequest(
+    `field ${field} is missing from data.`,
+    null
+  );
+
+  if (typeof data === "object") {
+    //check if fields in rule objects exist in data object
+    const hasField = (field: string, data: any): void => {
+      if (!(field in data)) {
+        throw fieldError;
+      }
+    };
+    //check for nested fields
+    if (field.includes(".")) {
+      const keys: string[] = field.split(".");
+      if (keys[0] in data) {
+        hasField(keys[1], data[keys[0]]);
+      } else throw fieldError;
+    } else hasField(field, data);
+  }
+};
+
 export const validateRequiredFields = (
   { body }: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const { rule, data }: any = body;
   try {
-    validateRule(body.rule);
-    validateData(body.data);
+    validateRule(rule);
+    validateData(data);
+    checkFields(rule.field, data);
     next();
   } catch (err) {
     next(err);
